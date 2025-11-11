40c84d0a70595d12445bec0fc8e85a0875917815
clare <civers@machealth.com.au>
2025-11-11 14:35:54 +1100

    Compose tabs in exercises component into reusable component with slots

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { CORE_CONCEPTS } from "../data";
 import Section from "./UI/Section";
+import Tabs from "./UI/Tabs";
 
 export default function Examples() {
   const [currentTab, setCurrentTab] = useState(0);
@@ -10,15 +11,13 @@ export default function Examples() {
 
   return (
     <Section id='examples' title='Examples'>
-      <menu>
-        {CORE_CONCEPTS.map(c => <button className={c.id === currentTab ? 'active' : ''} onClick={() => handleButtonClick(c.id)} key={c.id}>{c.title}</button>)}
-      </menu>
-
-      <div id="tab-content">
-        <h3>{CORE_CONCEPTS[currentTab].title}</h3>
-        <p>{CORE_CONCEPTS[currentTab].exampleDescription}</p>
-        <pre>{CORE_CONCEPTS[currentTab].code}</pre>
-      </div>
+      <Tabs buttons={CORE_CONCEPTS.map(c => <button className={c.id === currentTab ? 'active' : ''} onClick={() => handleButtonClick(c.id)} key={c.id}>{c.title}</button>)}>
+        <div id="tab-content">
+          <h3>{CORE_CONCEPTS[currentTab].title}</h3>
+          <p>{CORE_CONCEPTS[currentTab].exampleDescription}</p>
+          <pre>{CORE_CONCEPTS[currentTab].code}</pre>
+        </div>
+      </Tabs>
 
     </Section>
   )
