7dae171838ed740451b378440db2798e4c65e865
clare <civers@machealth.com.au>
2025-11-11 15:56:57 +1100

    formatting

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -5,13 +5,12 @@ import Tabs from "./UI/Tabs";
 
 export default function Examples() {
   const [currentTab, setCurrentTab] = useState(0);
-  const handleButtonClick = (id) => {
-    setCurrentTab(id);
-  }
+
+  const handleButtonClick = id => setCurrentTab(id);
 
   return (
     <Section id='examples' title='Examples'>
-      <Tabs ButtonsContainer='menu' buttons={CORE_CONCEPTS.map(c => <button className={c.id === currentTab ? 'active' : ''} onClick={() => handleButtonClick(c.id)} key={c.id}>{c.title}</button>)}>
+      <Tabs buttons={CORE_CONCEPTS.map(c => <button className={c.id === currentTab ? 'active' : ''} onClick={() => handleButtonClick(c.id)} key={c.id}>{c.title}</button>)}>
         <div id="tab-content">
           <h3>{CORE_CONCEPTS[currentTab].title}</h3>
           <p>{CORE_CONCEPTS[currentTab].exampleDescription}</p>
